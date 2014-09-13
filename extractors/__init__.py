62db0ff44a235af3d6a8ab5a2e95b2cc868fafbb
Aneesh Agrawal <aneeshusa@gmail.com>
2014-09-13 12:06:59 -0400

    Add title (event name) extractor. -Only supports one event/email -Extracts the subject -Uses a regex to remove bracketed terms (i.e. mailing-list titles) --Fails with nested brackets

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,4 +1,6 @@
 import date_and_time_extractor
+import title_extractor
+
 from base_extractor import NotAnEventException
 
-extractors = [date_and_time_extractor.DateAndTimeExtractor]
+extractors = [date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor]
---
+++ extractors/title_extractor.py
@@ -0,0 +1,10 @@
+import re
+
+from base_extractor import BaseExtractor
+
+class TitleExtractor(BaseExtractor):
+    @staticmethod
+    def extract(message):
+       subject = message.get_header('Subject')
+       unbracketed = re.sub(r'\[.*?\]', r'', subject)
+       return {'title': unbracketed}
