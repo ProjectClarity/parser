52dfe5834aa32d4423bfee199bc8cd5c7cae2506
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 16:12:57 -0400

    fix imports

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,7 +1,4 @@
-import date_and_time_extractor
-import title_extractor
-import link_extractor
-
+import date_and_time_extractor, title_extractor, link_extractor
 from base_extractor import NotAnEventException
 
 extractors = [date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, link_extractor.LinkExtractor]
