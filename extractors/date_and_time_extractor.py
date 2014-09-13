fc7c9e7265a34667cb638f86b49c1387afec0783
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 11:18:57 -0400

    Initial Message class

--- extractors/date_and_time_extractor.py
+++ extractors/date_and_time_extractor.py
@@ -1,4 +1,9 @@
+import parsedatetime as pdt
+
 def DateAndTimeExtractor(BaseExtractor):
     @staticmethod
-    def extract(data):
-      pass
+    def extract(message):
+      body = message.get('body')
+      cal = pdt.Calendar()
+      start_date = cal.parse(body.get_header('Date'))
+
