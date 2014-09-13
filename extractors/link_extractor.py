8dcf9f7e952443258bd7c2acca31531abfea7c29
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 16:09:14 -0400

    Fix up link_extractor

--- extractors/link_extractor.py
+++ extractors/link_extractor.py
@@ -1,36 +1,33 @@
-import re
-import urllib2
-
+import re, urllib2
 from base_extractor import BaseExtractor
 
-whitelist = [re.compile(regex) for regex in ['list-manage\d\.com']]
+URL_REGEX = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
 
-class CustomHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
-    def redirect_request(self, req, fp, code, msg, headers, newurl):
-        newreq = urllib2.HTTPRedirectHandler.redirect_request(self,
-            req, fp, code, msg, headers, newurl)
-        if newreq is not None:
-            self.redirections.append(newreq.get_full_url())
-        return newreq
+class HeadRequest(urllib2.Request):
+  def get_method(self):
+      return "HEAD"
 
-def follow(link):           
-    if any(regex.search(link) for regex in whitelist):
-        redirect_handler = CustomHTTPRedirectHandler()
-        redirect_handler.max_redirections = 100
-        redirect_handler.redirections = [link]
-        opener = urllib2.build_opener(redirect_handler)
-        request = urllib2.Request(link)
-        request.get_method = lambda : 'HEAD'
-        response = opener.open(request)
-        return redirect_handler.redirections[-1]
+class HTTPRedirectHandler(urllib2.HTTPRedirectHandler):
+  def redirect_request(self, req, fp, code, msg, headers, newurl):
+    newreq = urllib2.HTTPRedirectHandler.redirect_request(self,
+        req, fp, code, msg, headers, newurl)
+    if newreq is not None:
+        self.redirections.append(newreq.get_full_url())
+    return newreq
+
+def get_last_redirect(link):
+  redirect_handler = HTTPRedirectHandler()
+  redirect_handler.max_redirections = 100
+  redirect_handler.redirections = [link]
+  opener = urllib2.build_opener(redirect_handler)
+  request = HeadRequest(link)
+  opener.open(request)
+  return redirect_handler.redirections[-1]
 
 class LinkExtractor(BaseExtractor):
-    @staticmethod
-    def extract(message):
-       body = message # message.get('payload').get('body')
-       url_pattern = re.compile(r'((\S+?\.)+(\S+))')
-       matches = url_pattern.findall(body)
-       if len(matches) == 0:
-           return {}, {'links':[]}
-       links = [follow(match[0]) for match in matches]
-       return {}, {'links': links}
+  @staticmethod
+  def extract(message):
+   body = message.get('payload').get('body')
+   matches = URL_REGEX.findall(body)
+   links = [get_last_redirect(match[0]) for match in matches]
+   return {}, {'links': links}
