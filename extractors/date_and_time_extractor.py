98cf5f2dbc84d0a92236e55b60ee20853bd829ca
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 19:54:21 -0400

    Better heuristics for DateAndTimeExtractor

--- extractors/date_and_time_extractor.py
+++ extractors/date_and_time_extractor.py
@@ -2,6 +2,9 @@ from base_extractor import BaseExtractor
 import parsedatetime as pdt
 from email.utils import parsedate
 
+def validate_date(date):
+  return not date[-1].isdigit() and not date[-1].startswith('--') and any([x in date[-1] for x in [' ', '-', '/']])
+
 class DateAndTimeExtractor(BaseExtractor):
     @staticmethod
     def extract(message, context):
@@ -12,8 +15,20 @@ class DateAndTimeExtractor(BaseExtractor):
         DateAndTimeExtractor.throw()
       start_date = parsedate(date_header)
       dates = cal.nlp(body, sourceTime=start_date)
-      if dates and not dates[0][-1].isdigit() and not dates[0].startswith('--') and any([x in dates[0] for x in [' ', '-', '/']]):
-        return {'datetime': dates[0]}, {}
+      if dates:
+        dates = filter(lambda x: validate_date(x), dates)
+        for date in dates:
+          if date[-1][0].isdigit():
+            return {'datetime': date}, {}
+        for date in dates:
+          if date[-1][0].isupper():
+            return {'datetime': date}, {}
+        for date in dates:
+          if date[-1][0] not in ['tonight', 'today', 'tomorrow']:
+            return {'datetime': date}, {}
+        if dates:
+          return {'datetime': dates[0]}, {}
+        DateAndTimeExtractor.throw()
       else:
         DateAndTimeExtractor.throw()
 
