904aad68e450758d0d47884b932fc94ddefaa739
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 13:12:21 -0400

    Clean up code

--- pipeline.py
+++ pipeline.py
@@ -5,7 +5,7 @@ from mongo import get_raw_email, store_processed_data, DuplicateException
 from extractors import extractors, NotAnEventException
 from message import Message
 
-def process_email(raw_email):
+def get_events_from_email(raw_email):
     message = Message(raw_email)
     # Segmenting goes here
     results = {}
@@ -13,27 +13,21 @@ def process_email(raw_email):
     results['user_id'] = raw_email['userid']
     for extractor in extractors:
         results.update(extractor.extract(message))
-    return results
+    return [results]
 
 def post_events(event_ids):
     payload = {'event_ids': event_ids}
-
     req = urllib2.Request('http://pennappsx-web.herokuapp.com/events/create')
     req.add_header('Content-Type', 'application/json')
-
-    urlopen(req, json.dumps(payload))
+    urllib2.urlopen(req, json.dumps(payload))
 
 
 def process_notification(notification):
     object_id = notification['object_id']
     try:
         with get_raw_email(object_id) as raw_email:
-            processed_email = process_email(raw_email)
-            event_ids = []
-            for i in xrange(1): # Change to actually loop over all events
-                event_object_id = store_processed_data(processed_email)
-                event_ids.append(str(event_object_id))
-            post_events(event_ids)
+            events = get_events_from_email(raw_email)
+            post_events([store_processed_data(event) for event in events])
     except (DuplicateException, NotAnEventException):
         pass
     except:
