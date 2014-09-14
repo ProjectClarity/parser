00a32bb38020636ac80f7fba33dcf5b33068fc1d
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-14 09:11:41 -0400

    add CNN to blacklist

--- extractors/source_extractor.py
+++ extractors/source_extractor.py
@@ -6,7 +6,7 @@ class SourceExtractor(BaseExtractor):
     def extract(message, context):
       _from = message.get_header('From')
       from_name, from_email = email.utils.parseaddr(_from)
-      if from_email.split('@')[-1].lower() in ['facebookmail.com', 'twitter.com', 'meetup.com']:
+      if from_email.split('@')[-1].lower() in ['facebookmail.com', 'twitter.com', 'meetup.com', 'mail.cnn.com']:
         SourceExtractor.throw()
       if from_name:
         return {'source': from_name, 'email': from_email}, {}
