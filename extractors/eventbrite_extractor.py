160aa85395f6d3c07eabe9f371a23ad95260628c
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-14 03:05:53 -0400

    put lat/lon back

--- extractors/eventbrite_extractor.py
+++ extractors/eventbrite_extractor.py
@@ -24,5 +24,7 @@ class EventbriteExtractor(BaseExtractor):
         end = soup.find('span', class_='dtend')
         timezone = message.get_header('X-Time-Zone')
         location = maybe_content(soup.find('meta', property='og:street-address'))
-        return {'eventbrite_url': link, 'url': link, 'datetime': [naive_datetime_from_span(start, timezone)], 'end': [naive_datetime_from_span(end, timezone)], 'location': location}, {}
+        latitude = maybe_content(soup.find('meta', property='og:latitude'))
+        longitude = maybe_content(soup.find('meta', property='og:longitude'))
+        return {'eventbrite_url': link, 'url': link, 'datetime': [naive_datetime_from_span(start, timezone)], 'end': [naive_datetime_from_span(end, timezone)], 'location': location, 'latitude': latitude, 'longitude': longitude}, {}
     return {}, {}
