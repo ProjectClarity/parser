d6c3dfba3e1bde3c8ccf52fc7513f5817654e22d
Aneesh Agrawal <aneeshusa@gmail.com>
2014-09-14 06:38:20 +0000

    Add link extractor.

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,6 +1,7 @@
 import date_and_time_extractor
 import title_extractor
+import link_extractor
 
 from base_extractor import NotAnEventException
 
-extractors = [date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor]
+extractors = [date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, link_extractor.LinkExtractor]
---
+++ extractors/link_extractor.py
@@ -0,0 +1,34 @@
+import re
+import urllib2
+
+from base_extractor import BaseExtractor
+
+whitelist = [re.compile(regex) for regex in ['list-manage\d\.com']]
+
+class CustomHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
+    def redirect_request(self, req, fp, code, msg, headers, newurl):
+        newreq = urllib2.HTTPRedirectHandler.redirect_request(self,
+            req, fp, code, msg, headers, newurl)
+        if newreq is not None:
+            self.redirections.append(newreq.get_full_url())
+        return newreq
+
+class LinkExtractor(BaseExtractor):
+    @staticmethod
+    def extract(message):
+       body = message # message.get('payload').get('body')
+       url_pattern = re.compile(r'((\S+?\.)+(\S+))')
+       matches = url_pattern.findall(body)
+       if len(matches) == 0:
+           return {}, {'links':[]}
+       links = [match[0] for match in matches]
+       for link in links:
+           if any(regex.search(link) for regex in whitelist):
+               redirect_handler = CustomHTTPRedirectHandler()
+               redirect_handler.max_redirections = 100
+               redirect_handler.redirections = [link]
+               opener = urllib2.build_opener(redirect_handler)
+               request = urllib2.Request(link)
+               request.get_method = lambda : 'HEAD'
+               response = opener.open(request)
+       return {}, {'links': links}
