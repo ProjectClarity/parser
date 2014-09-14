57319a4c7bbd40fcfd4f91f971fe59e0385bf5c6
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 20:29:13 -0400

    Add anti-link heuristic to validate_date

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,4 +1,4 @@
 import date_and_time_extractor, title_extractor, link_extractor, address_extractor
 from base_extractor import NotAnEventException
 
-extractors = [date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, link_extractor.LinkExtractor, address_extractor.AddresssExtractor]
+extractors = [link_extractor.LinkExtractor, date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, address_extractor.AddresssExtractor]
--- extractors/date_and_time_extractor.py
+++ extractors/date_and_time_extractor.py
@@ -2,8 +2,10 @@ from base_extractor import BaseExtractor
 import parsedatetime as pdt
 from email.utils import parsedate
 
-def validate_date(date):
+def validate_date(date, context):
   date_text = date[-1]
+  if any([date_text in x for x in context['links']]):
+    return False
   if "\n" in date_text:
     return False
   if len(date_text) < 5 and not any([x in date_text for x in ['-', '/', 'am', 'pm']]):
@@ -21,7 +23,7 @@ class DateAndTimeExtractor(BaseExtractor):
       start_date = parsedate(date_header)
       dates = cal.nlp(body, sourceTime=start_date)
       if dates:
-        dates = list(filter(lambda x: validate_date(x), dates))
+        dates = list(filter(lambda x: validate_date(x, context), dates))
         dates.sort(key=lambda x: len(x[-1]))
         for date in dates:
           if date[-1][0].isdigit():
