d1d1d97744756a512faa5afbe586d3128d4a782b
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 22:26:18 -0400

    Add source_extractor

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,5 +1,5 @@
-import date_and_time_extractor, title_extractor, link_extractor, address_extractor, eventbrite_extractor, mit_location_extractor
+import date_and_time_extractor, title_extractor, link_extractor, address_extractor, eventbrite_extractor, mit_location_extractor, source_extractor
 from base_extractor import NotAnEventException
 
-extractors = [link_extractor.LinkExtractor, date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, address_extractor.AddresssExtractor, eventbrite_extractor.EventbriteExtractor, mit_location_extractor.MITLocationExtractor]
+extractors = [source_extractor.SourceExtractor, link_extractor.LinkExtractor, date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, address_extractor.AddresssExtractor, eventbrite_extractor.EventbriteExtractor, mit_location_extractor.MITLocationExtractor]
 
