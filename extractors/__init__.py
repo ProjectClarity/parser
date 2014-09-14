edd5bff5b98420e6c1ffbaa2b59805cc6f61c068
Aneesh Agrawal <aneeshusa@gmail.com>
2014-09-14 10:19:38 +0000

    Add MIT Location extractor. Runs after location has been set and looks for locations that are in buildings at MIT. -Uses the whereis.mit.edu search API.

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,4 +1,4 @@
-import date_and_time_extractor, title_extractor, link_extractor, eventbrite_extractor
+import date_and_time_extractor, title_extractor, link_extractor, eventbrite_extractor, mit_location_extractor
 from base_extractor import NotAnEventException
 
-extractors = [date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, link_extractor.LinkExtractor, eventbrite_extractor.EventbriteExtractor]
+extractors = [date_and_time_extractor.DateAndTimeExtractor, title_extractor.TitleExtractor, link_extractor.LinkExtractor, eventbrite_extractor.EventbriteExtractor, mit_location_extractor.MITLocationExtractor]
