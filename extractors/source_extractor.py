67708ca01f4060000f71eb4ed048289de72bf7c1
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 21:47:55 -0400

    add SourceExtractor

---
+++ extractors/source_extractor.py
@@ -0,0 +1,15 @@
+from base_extractor import BaseExtractor
+import email.utils
+
+class SourceExtractor(BaseExtractor):
+    @staticmethod
+    def extract(message, context):
+      _from = message.get_header('From')
+      from_name, from_email = email.utils.parseaddr(_from)
+      if from_email.split('@')[-1].lower() in ['facebookmail.com', 'twitter.com', 'meetup.com']:
+        SourceExtractor.throw()
+      if from_name:
+        return {'source': from_name}, {}
+      else:
+        return {}, {}
+
