a321116e56d7a0e2d546ea8a481f9d7f05b37129
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-15 13:42:38 -0400

    Make source and title exceptions less aggressive

--- extractors/source_extractor.py
+++ extractors/source_extractor.py
@@ -4,9 +4,13 @@ import email.utils
 class SourceExtractor(BaseExtractor):
     @staticmethod
     def extract(message, context):
-      _from = message.get_header('From')
+      _from = message.get_header('From').lower()
       from_name, from_email = email.utils.parseaddr(_from)
-      if from_email.split('@')[-1].lower() in ['facebookmail.com', 'twitter.com', 'meetup.com', 'mail.cnn.com']:
+      if from_email in ['info@twitter.com', 'calendar-notification@google.com']:
+        SourceExtractor.throw()
+      if any(x in from_email.split('@')[0].replace('-','') for x in ['noreply']):
+        SourceExtractor.throw()
+      if from_email.split('@')[-1] in ['facebookmail.com', 'mail.cnn.com']:
         SourceExtractor.throw()
       if from_name:
         return {'source': from_name, 'email': from_email}, {}
--- extractors/title_extractor.py
+++ extractors/title_extractor.py
@@ -5,7 +5,7 @@ class TitleExtractor(BaseExtractor):
     @staticmethod
     def extract(message, context):
        subject = message.get_header('Subject')
-       if any([x in subject.lower() for x in ['digest', 'activate', 'agenda']]):
+       if any([x in subject.lower() for x in ['digest', 'activate', 'agenda', 'new meetup group']]):
         TitleExtractor.throw()
        unbracketed = re.sub(r'\[.*?\]', r'', subject)
        return {'title': unbracketed}, {}
